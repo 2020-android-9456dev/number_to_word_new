5db9bb6be3eccd0337a651ee2e564e7e52db994f
umesh <umeshdexter32@gmail.com>
2020-01-20 23:04:53 +0530

    Remove Example Unint Test & Create New Class of CheckNumberRangeTest

---
+++ app/src/test/java/com/example/numbertowordconvertornew/CheckNumberRangeTest.kt
@@ -0,0 +1,19 @@
+package com.example.numbertowordconvertornew
+
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import org.junit.Before
+import org.junit.Test
+
+class CheckNumberRangeTest{
+
+    @Before
+    fun setup() {
+
+    }
+
+    @Test
+    fun check_withEmptyNumber_callShowConvertedWord(){
+
+    }
+}
