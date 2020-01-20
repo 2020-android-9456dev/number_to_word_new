dd70184a5c991cc91230570688e1805dc8ac4bbf
umesh <umeshdexter32@gmail.com>
2020-01-20 23:08:18 +0530

    Create NumberToWordConvertorPresenter class As per MVP Pattern

---
+++ app/src/main/java/com/example/numbertowordconvertornew/NumberToWordConvertorPresenter.kt
@@ -0,0 +1,29 @@
+package com.example.numbertowordconvertornew
+
+import android.view.View
+
+class NumberToWordConvertorPresenter {
+
+    private var view: View? = null
+
+    fun attachView(view: View) {
+        this.view = view
+    }
+
+    fun detachView() {
+        this.view = null
+    }
+
+    fun convertNumberToWord(number: String) {
+        if (number.trim().isBlank() || number.toInt()==0) {
+            view?.checkNumberRange()
+        } else {
+            view?.showConvertedWord(number)
+        }
+    }
+
+    interface View {
+        fun checkNumberRange()
+        fun showConvertedWord(query: String)
+    }
+}
