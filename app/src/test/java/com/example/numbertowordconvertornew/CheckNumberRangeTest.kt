bd01ef2b8678c2bfe02fa4343af8d70cae89943e
umesh <umeshdexter32@gmail.com>
2020-01-20 23:13:26 +0530

    Modify checkNumberRangeTest class for unit test case creation

--- app/src/test/java/com/example/numbertowordconvertornew/CheckNumberRangeTest.kt
+++ app/src/test/java/com/example/numbertowordconvertornew/CheckNumberRangeTest.kt
@@ -7,13 +7,20 @@ import org.junit.Test
 
 class CheckNumberRangeTest{
 
+    private lateinit var presenter : NumberToWordConvertorPresenter
+    private lateinit var view : NumberToWordConvertorPresenter.View
+
     @Before
     fun setup() {
-
+        presenter = NumberToWordConvertorPresenter()
+        view = mock()
+        presenter.attachView(view)
     }
 
     @Test
     fun check_withEmptyNumber_callShowConvertedWord(){
+        presenter.convertNumberToWord("")
 
+        verify(view).checkNumberRange()
     }
 }
